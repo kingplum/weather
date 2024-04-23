2ab89a2623f3dbd20340ad02048a89cb837c10e9
Hoang Nguyen <nguyenhoangcit@gmail.com>
2024-04-23 17:28:36 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // main js
+jQuery(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        asNavFor: '.slider-nav',
+        rows: 0,
+        draggable: false,
+        infinite: false
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        rows: 0,
+        draggable: false,
+        infinite: false
+    });
+});
