7c739e08084697cf1dfea6ba415ac317d9d1693e
Hoang Nguyen <nguyenhoangcit@gmail.com>
2024-04-24 11:14:35 +0700

    new update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,47 @@
 // main js
 jQuery(document).ready(function(){
     $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        asNavFor: '.slider-nav',
-        rows: 0,
-        draggable: false,
-        infinite: false
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: false,
-        rows: 0,
-        draggable: false,
-        infinite: false
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      asNavFor: '.slider-nav',
+      rows: 0,
+      infinite: false,
+      swipe: false,
+      swipeToSlide: false,
+      touchMove: false,
+      draggable: false,
+      accessibility: false
     });
+    $('.slider-nav').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      rows: 0,
+      infinite: false,
+      swipe: false,
+      swipeToSlide: false,
+      touchMove: false,
+      draggable: false,
+      accessibility: false
+    });
+    // $.fn.mousedownScroll = function(data, fn) {
+    //   if ( fn == null ) {
+    //       fn = data;
+    //       data = null;
+    //   }    
+    //   var o = fn;
+    //   fn = function(e){
+    //       if(inScrollRange(e)) {
+    //           e.type = "mousedownscroll";
+    //           return o.apply(this, arguments);
+    //       }
+    //       return;
+    //   };
+    //   if ( arguments.length > 0 ) {
+    //       return this.bind( "mousedown", data, fn );
+    //   } 
+    //   return this.trigger( "mousedown" );
+    // };
 });
