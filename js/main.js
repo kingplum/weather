f3769bbb7b12f1736eb9988a9ec766f0ed6dc225
DESKTOP-KFGVGP3\ADMIN <nguyenhoangcit@gmail.com>
2024-05-05 09:03:06 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -183,8 +183,9 @@ jQuery(document).ready(function(){
               } else {
                 cls = '';
               }
-
-              cols += '<div id="col-'+d+'-'+id+'" class="'+cls+'"> <div class="wicon"><img src="'+img+'"></div> <div class="temp" '+tls+'>'+temp+'</div> <div class="wind">'+wind+'</div> <div class="time">'+surt+'</div> <div><span class="chart" '+ss+'></span></div> </div>';
+              if($.inArray(id, [0,1,2,21,22,23]) === -1) {
+                cols += '<div id="col-'+d+'-'+id+'" class="'+cls+'"> <div class="wicon"><img src="'+img+'"></div> <div class="temp" '+tls+'>'+temp+'</div> <div class="wind">'+wind+'</div> <div class="time">'+surt+'</div> <div><span class="chart" '+ss+'></span></div> </div>';
+              }
             });
             $('.slider-nav').append('<div class="wrap-weather"> <h2>'+m+'/'+d+'('+getdayofweek(day)+')<span>'+formatAMPM(new Date)+'</span></h2> <p><span class="sunny icon-time"><img src="images/sunny.png" alt="">日の出'+rh+':'+rm+'</span> <span class="night icon-time"><img src="images/night-mode.png" alt="">日の入り'+sh+':'+sm+'</span></p> </div>');
             $('.slider-for').append('<div> <div class="sec1 d-flex r-center"> <div class="gap1"> <div> <img src="'+cwicon+'" alt=""> <div><span>'+ctemp+'</span>&deg;C</div> </div> </div> <div  class="gap2"> <span>風向.風速(m/s)</span> <strong><big>'+csurt+'</big> m/s</strong> </div> <div  class="gap3"> '+cwind+' </div> <div  class="gap4"> <strong>'+cdirec+'</strong> <span>'+ctype+'</span> </div> </div> <div class="sec2 d-flex r-center"> <div class="item gap2"> <span>波高(m)</span> <h4><big>'+misurt+'-'+masurt+'</big>m</h4> </div> <div class="item"> '+ts1+ts2+ts3+' </div> </div> <div class="sec3"> <div class="d-flex tbl"> <p><span>オン</span>/<span>サイド</span>/<span>オフ</span></p> '+cols+' <div class="line-chart"> <div class="aspect-ratio"> <canvas id="charts-'+d+'"></canvas> </div> </div> </div> </div> </div>');
@@ -211,6 +212,13 @@ jQuery(document).ready(function(){
               options: {
                 responsive: true,
                 maintainAspectRatio: false,
+                layout: {
+                  padding: {
+                    top: 20,
+                    left: 5,
+                    right: 25
+                  }
+                },
                 animation: {
                     easing: 'easeInOutQuad',
                     duration: 450,
@@ -226,8 +234,8 @@ jQuery(document).ready(function(){
                           var view = elements[j]._view;                  
                           var text = configOptions.barGoals ? datasets[i].rawData[j] : datasets[i].data[j];
                           var textWidth = ctx.measureText(text).width;
-                          var x = view.x - (textWidth / 2) + 5;
-                          var y = view.y;
+                          var x = view.x - (textWidth / 2);
+                          var y = view.y - 10;
                           ctx.save();
                           ctx.fillText(text + 'm', x, y);
                           ctx.restore();
@@ -252,9 +260,7 @@ jQuery(document).ready(function(){
                     },
                     ticks: {
                       fontColor: "#fff",
-                      display: false,
-                      beginAtZero: true,
-                      padding: 25
+                      display: false
                     }
                   }]
                 },
